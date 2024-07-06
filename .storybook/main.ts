681f2e8d5848ad22817da142e930bd9f7130f020
FATESAIKOU <qzsecftbhhhh@gmail.com>
2024-07-06 18:05:41 +0900

    Refine storybook addon and add viewpoint support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,10 @@ import type { StorybookConfig } from "@storybook/svelte-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport",
   ],
   framework: {
     name: "@storybook/svelte-vite",
