65fa4f4b816d258fe645c83fd31b5c4999262f99
FATESAIKOU <qzsecftbhhhh@gmail.com>
2024-07-06 18:11:56 +0900

    Add custom viewpoint

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,43 @@
 import type { Preview } from "@storybook/svelte";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  smallMobile: {
+    name: 'Small Mobile',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+  },
+  largeMobile: {
+    name: 'Large Mobile',
+    styles: {
+      width: '414px',
+      height: '896px',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  smallLaptop: {
+    name: 'Small Laptop',
+    styles: {
+      width: '1366px',
+      height: '768px',
+    },
+  },
+  largeLaptop: {
+    name: 'Large Laptop',
+    styles: {
+      width: '1920px',
+      height: '1080px',
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +47,13 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...customViewports,
+      },
+      defaultViewport: 'responsive',
+    },  
   },
 };
 
